fdb4a218e739d326a234eccd55b5b5790c479b8b
Fabian <bian251091@gmail.com>
2020-02-09 04:15:23 -0300

    form with validation

--- src/Components/Form/beneficiaryForm.jsx
+++ src/Components/Form/beneficiaryForm.jsx
@@ -7,7 +7,9 @@ import Select from '@material-ui/core/Select';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import { Formik } from 'formik';
+import FormHelperText from '@material-ui/core/FormHelperText';
 import { createBeneficiary } from '../../Services/Calls'
+import * as Yup from 'yup';
 
 const useStyles = makeStyles(theme => ({
   formControl: {
@@ -31,25 +33,57 @@ export default function BeneficiaryForm(props) {
     setLabelWidth(inputLabel.current.offsetWidth);
   }, []);
 
-  const handleChangeBank = event => {
+  const handleChange = event => {
     setBank(event.target.value);
   };
+  const SignupSchema = Yup.object().shape({
+    account_number: Yup.string()
+      .min(2, 'Too Short!')
+      .max(50, 'Too Long!')
+      .required('Required'),
+    name: Yup.string()
+      .min(2, 'Too Short!')
+      .max(50, 'Too Long!')
+      .required('Required'),
+    last_name: Yup.string()
+      .min(2, 'Too Short!')
+      .max(50, 'Too Long!')
+      .required('Required'),
+    bank: Yup.string()
+      .min(2, 'Too Short!')
+      .max(50, 'Too Long!')
+      .required('Required'),
+    personal_id: Yup.string()
+      .min(2, 'Too Short!')
+      .max(50, 'Too Long!')
+      .required('Required'),
+    telephone_number: Yup.string()
+      .min(2, 'Too Short!')
+      .max(50, 'Too Long!')
+      .required('Required'),
+    mobile_pay: Yup.string()
+      .min(2, 'Too Short!')
+      .max(50, 'Too Long!')
+      .required('Required'),
+  });
   return (
     <div>
       <Formik
-      initialValues={{ account_number: '', name: '', last_name: '', bank: bank,
+      initialValues={{ account_number: '', name: '', last_name: '', bank: '',
       personal_id: '',  telephone_number: '', mobile_pay: '' 
       }}
+      validationSchema={SignupSchema}
       onSubmit={(values, { setSubmitting }) => {
         setTimeout(() => {
           console.log(values);
-          createBeneficiary(user, setUser, values)
           setSubmitting(false);
         }, 400);
       }}
     >
       {({
         values,
+        errors,
+        touched,
         handleChange,
         handleBlur,
         handleSubmit,
@@ -58,50 +92,59 @@ export default function BeneficiaryForm(props) {
       }) => (
 <form onSubmit={handleSubmit}>
       <div>
-        <TextField helperText={''} error={false} label="Numero de cuenta" name="account_number" variant="outlined" onChange={handleChange}
+        <TextField error={errors.account_number && touched.account_number && errors.account_number !== null}
+            helperText={errors.account_number && touched.account_number && errors.account_number} label="Numero de cuenta" name="account_number" variant="outlined" onChange={handleChange}
             onBlur={handleBlur}
             value={values.account_number}/>
       </div>
       <div>
-        <TextField label="Nombre" name="name" variant="outlined" onChange={handleChange}
+        <TextField helperText={errors.name && touched.name && errors.name} 
+            error={errors.name && touched.name && errors.name !== null}
+            label="Nombre" name="name" variant="outlined" onChange={handleChange}
             onBlur={handleBlur}
             value={values.name}/>
       </div>
       <div>
-        <TextField label="Apellido" name="last_name" variant="outlined" onChange={handleChange}
+        <TextField helperText={errors.last_name && touched.last_name && errors.last_name}
+            label="Apellido" name="last_name" variant="outlined" onChange={handleChange}
+            error={errors.last_name && touched.last_name && errors.last_name !== null}
             onBlur={handleBlur}
             value={values.last_name}/>
       </div>
-      <FormControl variant="outlined" className={classes.formControl}>
+      <FormControl variant="outlined" className={classes.formControl} error={errors.bank && touched.bank && errors.bank !== null}>
         <InputLabel ref={inputLabel} id="demo-simple-select-outlined-label">
           Banco
         </InputLabel>
         <Select
+          name="bank"
           labelId="demo-simple-select-outlined-label"
           id="demo-simple-select-outlined"
-          value={bank}
-          onChange={handleChangeBank}
+          onChange={handleChange}
+          value={values.bank}      
           labelWidth={labelWidth}
         >
-          <MenuItem value="">
-          </MenuItem>
           <MenuItem value={"Mercantil"}>Mercantil</MenuItem>
           <MenuItem value={"Banesco"}>Banesco</MenuItem>
           <MenuItem value={"Provincial"}>Provincial</MenuItem>
         </Select>
+        <FormHelperText>{errors.bank && touched.bank && errors.bank}</FormHelperText>
       </FormControl>
       <div>
-        <TextField label="Cedula" name="personal_id" variant="outlined" onChange={handleChange}
+        <TextField helperText={errors.personal_id && touched.personal_id && errors.personal_id} label="Cedula" name="personal_id" variant="outlined" onChange={handleChange}
+            error={errors.personal_id && touched.personal_id && errors.personal_id !== null}
             onBlur={handleBlur}
             value={values.personal_id}/>
       </div>
       <div>
-        <TextField label="Numero de telefono" name="telephone_number" variant="outlined" onChange={handleChange}
+        <TextField helperText={errors.telephone_number && touched.telephone_number && errors.telephone_number}
+            error={errors.telephone_number && touched.telephone_number && errors.telephone_number !== null}
+            label="Numero de telefono" name="telephone_number" variant="outlined" onChange={handleChange}
             onBlur={handleBlur}
             value={values.telephone_number}/>
       </div>
       <div>
-        <TextField label="Pago Movil" name="mobile_pay" variant="outlined" onChange={handleChange}
+        <TextField helperText={errors.mobile_pay && touched.mobile_pay && errors.mobile_pay} label="Pago Movil" name="mobile_pay" variant="outlined" onChange={handleChange}
+            error={errors.mobile_pay && touched.mobile_pay && errors.mobile_pay !== null}
             onBlur={handleBlur}
             value={values.mobile_pay}/>
       </div>
