069625502df15209e3973db687eec4c6c4ccfdab
Fabian Hernandez <itwarp_fpacheco@iseatz.com>
2020-02-19 00:31:53 -0300

    navigation

--- src/Components/Header/Navigation.jsx
+++ src/Components/Header/Navigation.jsx
@@ -1,80 +1,113 @@
-import React from 'react';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import IconButton from '@material-ui/core/IconButton';
-import Typography from '@material-ui/core/Typography';
-import { fade, makeStyles } from '@material-ui/core/styles';
-import MenuIcon from '@material-ui/icons/Menu';
-import SearchIcon from '@material-ui/icons/Search';
-import SearchBar from "./Autocomplete";
-import MenuListComposition from "./Menu"
+import React from "react";
+import { fade, makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import InputBase from "@material-ui/core/InputBase";
+import SearchIcon from "@material-ui/icons/Search";
+import MenuListComposition from "./Menu";
 
 const useStyles = makeStyles(theme => ({
-  root: {
-    flexGrow: 1,
+  grow: {
+    flexGrow: 1
   },
   menuButton: {
-    marginRight: theme.spacing(2),
+    marginRight: theme.spacing(2)
   },
   title: {
-    flexGrow: 1,
-    display: 'none',
-    [theme.breakpoints.up('sm')]: {
-      display: 'block',
-    },
+    display: "none",
+    [theme.breakpoints.up("sm")]: {
+      display: "block"
+    }
   },
   search: {
-    position: 'relative',
+    position: "relative",
     borderRadius: theme.shape.borderRadius,
     backgroundColor: fade(theme.palette.common.white, 0.15),
-    '&:hover': {
-      backgroundColor: fade(theme.palette.common.white, 0.25),
+    "&:hover": {
+      backgroundColor: fade(theme.palette.common.white, 0.25)
     },
+    marginRight: theme.spacing(2),
     marginLeft: 0,
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      marginLeft: theme.spacing(1),
-      width: 'auto',
-    },
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      marginLeft: theme.spacing(3),
+      width: "auto"
+    }
   },
   searchIcon: {
     width: theme.spacing(7),
-    height: '100%',
-    position: 'absolute',
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center"
   },
   inputRoot: {
-    color: 'inherit',
+    color: "inherit"
   },
   inputInput: {
     padding: theme.spacing(1, 1, 1, 7),
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      width: 120,
-      '&:focus': {
-        width: 200,
-      },
-    },
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("md")]: {
+      width: 200
+    }
   },
+  sectionDesktop: {
+    display: "none",
+    [theme.breakpoints.up("md")]: {
+      display: "flex"
+    }
+  },
+  sectionMobile: {
+    display: "flex",
+    [theme.breakpoints.up("md")]: {
+      display: "none"
+    }
+  }
 }));
 
-export default function Navigation(props) {
+export default function PrimarySearchAppBar(props) {
+  const {
+    data,
+    temporalData,
+    filterTemporalData,
+    user,
+    setUser
+  } = props;
+
   const classes = useStyles();
-  const { data, temporalData, filterTemporalData, user, setUser} = props;
+
+  function filterResult(event, data) {
+    filterTemporalData(data.filter((el) => {
+      return el.personal_id.toString().startsWith(event.target.value)
+          }))   
+  }  
+
   return (
-    <div className={classes.root}>
+    <div className={classes.grow}>
       <AppBar position="static">
         <Toolbar>
-          <MenuListComposition user={user} setUser={setUser}/>
+          <MenuListComposition user={user} setUser={setUser} />
           <Typography className={classes.title} variant="h6" noWrap>
-            Kmbios_VE
+            Kmbios-VE
           </Typography>
           <div className={classes.search}>
-            <SearchBar data={data} temporalData={temporalData} filterTemporalData={filterTemporalData}/>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              placeholder="Buscar por cedula"
+              type="tel"
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput
+              }}
+              inputProps={{ "aria-label": "search" }}
+              onChange={(event)=> filterResult(event, data)}
+            />
           </div>
         </Toolbar>
       </AppBar>
